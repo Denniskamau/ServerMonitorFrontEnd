3bb72070e13c6c42a9c27d390c8c84bf8e43b573
Dennis <denniskamau3@gmail.com>
2018-09-01 17:53:50 +0300

    created the signup form fields

--- src/components/signup.js
+++ src/components/signup.js
@@ -2,22 +2,42 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form'
 
-let SignupForm = () =>
-    <form onSubmit={handleSubmit}>
-        <div>
-            <label>Email</label>
-            <Field name="email" component="input"></Field>
-        </div>
-        <div>
-            <label>Password</label>
-            <Field name="password" component="input"></Field>
-        </div>
-        <div>
-            <button type="submit">Submit</button>
-        </div>
+
+const validate = values => {
+    const errors = {}
+
+    if(!values.email) {
+        errors.email = 'Required'
+    }
+    if(!values.password) {
+        errors.password = 'Required'
+    }
+    return errors
+}
+
+const renderInput = ({ input , meta, label}) => {
+    <div>
+    <label>{ label}</label>
+    <input {...input}/>
+    {meta.error && meta.touched &&<span>{meta.error}</span>}
+</div>
+     
+}
+let SignupForm = ({ handleSubmit, submitting}) =>
+    <form >
+        
+        <Field name="email" label="Email" component={renderInput}></Field>
+        
+        
+        <Field name="password" label="Password" component={renderInput}></Field>
+        
+        <button type="submit" disabled={submitting}>Submit</button>
+
     </form>
 
 SignupForm = reduxForm({
-    form: 'signup'
+    form: 'signup',
+    destroyOnUnmount:false,
+    validate
 })(SignupForm)
 export default SignupForm;
