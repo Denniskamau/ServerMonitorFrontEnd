a8b949b3e37934cbb377d3e6f02495fdb3d75edd
Dennis <denniskamau3@gmail.com>
2018-09-08 12:24:21 +0300

    create signup action

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -3,7 +3,8 @@ import {
   LOGGING_USER_BEGIN,
   LOGGING_USER_SUCCESS,
   LOGGING_USER_FAILURE,
-  POST_USER   
+  POST_USER,
+  SIGNUP_USER   
 } from './types';
 
 export const  getUser = (data) => dispatch => {
@@ -25,4 +26,24 @@ export const  getUser = (data) => dispatch => {
           })
         })
       
+}
+
+export const signUpUser = (data) => dispatch => {
+  console.log('signup action')
+  fetch(
+    'http://localhost:8080/user/signup', { 
+      method: 'POST',
+      headers: {
+          'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  }
+  )
+  .then(res => res.json())
+  .then( user => {
+    dispatch({
+      type: SIGNUP_USER,
+      payload: user
+    })
+  })
 }
