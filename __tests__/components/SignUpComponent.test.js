de43d88a962ab4aeeb95915e0904fc91ff2e09ae
Dennis <denniskamau3@gmail.com>
2018-09-11 17:17:54 +0300

    render component test pass

--- __tests__/components/SignUpComponent.test.js
+++ __tests__/components/SignUpComponent.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import LoginForm from '../../src/components/login'
+import  Signup from '../../src/components/signup'
 
-describe('render login', ()=>{
+describe('render signup component', ()=>{
     it('should render correctly', ()=>{
-        const wrapper = shallow(<LoginForm/>)
+        const wrapper = shallow(<Signup/>)
         expect(wrapper).toMatchSnapshot();
     })
 
