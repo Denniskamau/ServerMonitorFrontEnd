c666eae30d20a01416b749403adbc6f7f33d33aa
Dennis <denniskamau3@gmail.com>
2018-09-11 19:55:21 +0300

    finish test for app component

---
+++ __tests__/components/AppComponent.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import  App from '../../src/App'
+import  Signup from '../../src/components/signup'
+import LoginForm from '../../src/components/login'
+
+describe('App', () => {
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = shallow(<App/>)
+    })
+    it('should render a <div />', () => {
+      expect(wrapper.find('div').length).toEqual(1)
+    })
+
+    it('should render sign up component', ()=>{
+        expect(wrapper.containsMatchingElement(<Signup />)).toEqual(true)
+    })
+
+    it('should render Login COmponent', ()=>{
+        expect(wrapper.containsMatchingElement(<LoginForm />)).toEqual(true)
+    })
+  });
+
+
