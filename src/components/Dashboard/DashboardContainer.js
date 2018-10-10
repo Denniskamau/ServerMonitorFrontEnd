bae04cac2710830c21d6cd344da71f1674161a2f
Dennis <denniskamau3@gmail.com>
2018-10-10 19:00:58 +0300

    add website validation

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -4,12 +4,32 @@ import WebsiteList from './WebsiteList'
 import WebsiteForm from './WebsiteForm'
 
 export  function validation(data){
+    let errors = {}
+    let isError = false
+    if(data.name === undefined && data.url === undefined){
+        errors.name = 'Required'
+        errors.url = 'Required'
+        isError = true
+    }
+    if( data.name === undefined){
+        errors.name = 'Required'
+        isError = true
+    }
+    if(data.url === undefined){
+        errors.url = 'Required'
+        isError = true
+    }else {
+        let newWebsite = {} 
+        newWebsite.name = data.name
+        newWebsite.url = data.url
+        //dispatch action to send data to backend
+    }
 
 }
 
 export const DashboardContainer = ({ handleSubmit}) => {
     const submitForm = (formValues) => {
-        let data ={}
+        validation(formValues)
     }
 
     return (
